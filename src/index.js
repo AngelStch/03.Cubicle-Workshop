b8e68163f12d6b0eb6734dc829ea6f9f1c5f7bfd
AngelStch <adstoichev@gmail.com>
2023-10-05 10:05:59 +0300

    update 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 const express = require("express")
 const app = express()
-const handlebars = require("express-handlebars")
-const path = require("path")
+const handlebarsConfig = require("./config/handlebarasConfig.js")
+const expressConfig = require("./config/expressConfig.js")
+const {PORT} = require("./constants.js")
 
-const PORT = 5050
-app.engine("hbs", handlebars.engine({extname: "hbs"}))
-app.set("view engine", "hbs")
-app.set("views", "src/views")
-
-const staticFiles = express.static(path.resolve(__dirname,"public"))
-app.use(staticFiles)
+handlebarsConfig(app)
+expressConfig(app)
 
 
 app.get("/", (req,res)=>{
