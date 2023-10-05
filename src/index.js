196beab162a475a08ecf355d80e632c833364345
AngelStch <adstoichev@gmail.com>
2023-10-05 09:42:54 +0300

    1 update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require("express")
 const app = express()
-const handlebars = require("assert-handlebars")
+const handlebars = require("express-handlebars")
+const path = require("path")
 
 const PORT = 5050
 app.engine("hbs", handlebars.engine({extname: "hbs"}))
-app.set("view engiine", "hbs")
+app.set("view engine", "hbs")
 app.set("views", "src/views")
 
+const staticFiles = express.static(path.resolve(__dirname,"public"))
+app.use(staticFiles)
+
+
 app.get("/", (req,res)=>{
-    res.send("helo")
+    res.render("index")
 })
 
 app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`))
