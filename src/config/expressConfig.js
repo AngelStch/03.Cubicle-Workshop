8e9ba9bae9dbbe7cfc7ff06a2e02a112988411ef
AngelStch <adstoichev@gmail.com>
2023-10-05 11:58:31 +0300

    update 4
    add uniq id package, templating fixed,  added create functionality

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -3,6 +3,6 @@ const path = require("path")
 
 const expressConfig = (app)=>{
     app.use(express.static(path.resolve(__dirname,"../public")))
-
+    app.use(express.urlencoded({extended: false}))
 }
 module.exports = expressConfig
