37c1327fe69303a5a310c983d05c8a123f4c36cf
AngelStch <adstoichev@gmail.com>
2023-10-09 22:19:13 +0300

    extract connection string to the config

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 const constants = {
-    PORT: 5050
+    PORT: 5050,
+     URL: "mongodb://127.0.0.1:27017/cubical-sep-2023"
+
 }
 
 module.exports = constants
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const express = require("express")
 const app = express()
 const handlebarsConfig = require("./config/handlebarasConfig.js")
 const expressConfig = require("./config/expressConfig.js")
-const {PORT} = require("./constants.js")
 const routes = require("./router.js")
+const dbConnect = require("./config/dbConfig.js")
 
+
+const {PORT} = require("./constants.js")
 handlebarsConfig(app)
 expressConfig(app)
 
+dbConnect()
+.then(() => console.log("Successfully connection"))
+.catch((err) => console.log(`Error while connecting in databse: ${err}`))
 app.use(routes)
 
 
