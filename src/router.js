f8847e1f7a03fb6d9d4e03f1b1177382d2609d26
AngelStch <adstoichev@gmail.com>
2023-10-11 23:00:41 +0300

    added accessories thing

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ const router= require("express").Router();
 
 const homeControler = require("./controllers/homeControler.js")
 const cubeControler = require("./controllers/cubeControler.js")
+const accessoryController =  require("./controllers/accessoryController.js")
 
 router.use(homeControler)
 router.use("/cubes",cubeControler)
+router.use("/accessories", accessoryController)
 router.get("*",(req,res) =>{
     res.redirect('/404')
 })
